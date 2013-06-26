d9e76d586094ee608e2d64df57a0e3d4a191fe63
Scott Coleman <scott.coleman@tee2.org>
2013-06-26 13:32:30 -0700

    Added copyright headers

--- src/com/t2/dataouthandler/DataOutHandler.java
+++ src/com/t2/dataouthandler/DataOutHandler.java
@@ -1,35 +1,30 @@
-/*****************************************************************
-DataOutHandler
-
-Copyright (C) 2011 The National Center for Telehealth and 
-Technology
-
-Eclipse Public License 1.0 (EPL-1.0)
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the Eclipse Public License as
-published by the Free Software Foundation, version 1.0 of the 
-License.
-
-The Eclipse Public License is a reciprocal license, under 
-Section 3. REQUIREMENTS iv) states that source code for the 
-Program is available from such Contributor, and informs licensees 
-how to obtain it in a reasonable manner on or through a medium 
-customarily used for software exchange.
-
-Post your updates and modifications to our GitHub or email to 
-t2@tee2.org.
-
-This library is distributed WITHOUT ANY WARRANTY; without 
-the implied warranty of MERCHANTABILITY or FITNESS FOR A 
-PARTICULAR PURPOSE.  See the Eclipse Public License 1.0 (EPL-1.0)
-for more details.
- 
-You should have received a copy of the Eclipse Public License
-along with this library; if not, 
-visit http://www.opensource.org/licenses/EPL-1.0
-
-*****************************************************************/
+/* T2AndroidLib-SG for Signal Processing
+ * 
+ * Copyright © 2009-2012 United States Government as represented by 
+ * the Chief Information Officer of the National Center for Telehealth 
+ * and Technology. All Rights Reserved.
+ * 
+ * Copyright © 2009-2012 Contributors. All Rights Reserved. 
+ * 
+ * THIS OPEN SOURCE AGREEMENT ("AGREEMENT") DEFINES THE RIGHTS OF USE, 
+ * REPRODUCTION, DISTRIBUTION, MODIFICATION AND REDISTRIBUTION OF CERTAIN 
+ * COMPUTER SOFTWARE ORIGINALLY RELEASED BY THE UNITED STATES GOVERNMENT 
+ * AS REPRESENTED BY THE GOVERNMENT AGENCY LISTED BELOW ("GOVERNMENT AGENCY"). 
+ * THE UNITED STATES GOVERNMENT, AS REPRESENTED BY GOVERNMENT AGENCY, IS AN 
+ * INTENDED THIRD-PARTY BENEFICIARY OF ALL SUBSEQUENT DISTRIBUTIONS OR 
+ * REDISTRIBUTIONS OF THE SUBJECT SOFTWARE. ANYONE WHO USES, REPRODUCES, 
+ * DISTRIBUTES, MODIFIES OR REDISTRIBUTES THE SUBJECT SOFTWARE, AS DEFINED 
+ * HEREIN, OR ANY PART THEREOF, IS, BY THAT ACTION, ACCEPTING IN FULL THE 
+ * RESPONSIBILITIES AND OBLIGATIONS CONTAINED IN THIS AGREEMENT.
+ * 
+ * Government Agency: The National Center for Telehealth and Technology
+ * Government Agency Original Software Designation: T2AndroidLib1021
+ * Government Agency Original Software Title: T2AndroidLib for Signal Processing
+ * User Registration Requested. Please send email 
+ * with your contact information to: robert.kayl2@us.army.mil
+ * Government Agency Point of Contact for Original Software: robert.kayl2@us.army.mil
+ * 
+ */
 package com.t2.dataouthandler;
 
 import java.text.SimpleDateFormat;
